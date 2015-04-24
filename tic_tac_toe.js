e91fffeba0925983a793974ab6d4f9d36371f434
Tiffany W <tiffany.wang777@gmail.com>
2015-04-23 22:42:36 -0700

    Implement player and computer moves

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,7 +1,19 @@
+var drawMove = function(square, move){
+	$(square).removeClass("blank").addClass(move);
+};
+
+var generateComputerMove = function(){
+	var squares = $(".blank.square");
+	var square = squares[Math.floor(Math.random()*squares.length)];
+	return square;
+};
+
 var newGame = function(){
 	$(".blank").click(function(){
+		drawMove(this, "x");
+		var computerMove = generateComputerMove();
+		drawMove(computerMove, "o");
 	});
-
 };
 
 
