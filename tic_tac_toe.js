67060cf7bb5e8298e97974de409969d69bf02a0c
Tiffany W <tiffany.wang777@gmail.com>
2015-04-26 16:25:47 -0700

    implement check win

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -16,18 +16,108 @@ var newGame = function(){
 	});
 };
 
-
-//var checkEndGame = function(board, move){
-	//if() {
-		//console.log("Player wins!");
-	//} else if (){
-		//console.log("Computer wins!");
-	//} else{
+/*
+var checkEndGame = function(board, move){
+	if() {
+		console.log("Player wins!");
+	} else if (){
+		console.log("Computer wins!");
+	} else{
 
 	};
+};*/
 
+var checkRow = function(squares){
+	if($(squares[0]).hasClass("x") && 
+	   $(squares[1]).hasClass("x") && 
+	   $(squares[2]).hasClass("x")) {
+		return "x"
+	}
+	if($(squares[3]).hasClass("x") && 
+	   $(squares[4]).hasClass("x") && 
+	   $(squares[5]).hasClass("x")) {
+		return "x"
+	}
+	if($(squares[6]).hasClass("x") && 
+	   $(squares[7]).hasClass("x") && 
+	   $(squares[8]).hasClass("x")) {
+		return "x"
+	} 
+	if($(squares[0]).hasClass("o") && 
+	   $(squares[1]).hasClass("o") && 
+	   $(squares[2]).hasClass("o")) {
+		return "o"
+	}
+	if($(squares[3]).hasClass("o") && 
+	   $(squares[4]).hasClass("o") && 
+	   $(squares[5]).hasClass("o")) {
+		return "o"
+	}
+	if($(squares[6]).hasClass("o") && 
+	   $(squares[7]).hasClass("o") && 
+	   $(squares[8]).hasClass("o")) {
+		return "o"
+	} 
+	return null
+};
 
+var checkColumn = function(squares){
+	if($(squares[0]).hasClass("x") && 
+	   $(squares[3]).hasClass("x") && 
+	   $(squares[6]).hasClass("x")) {
+		return "x"
+	}
+	if($(squares[1]).hasClass("x") && 
+	   $(squares[4]).hasClass("x") && 
+	   $(squares[7]).hasClass("x")) {
+		return "x"
+	}
+	if($(squares[2]).hasClass("x") && 
+	   $(squares[5]).hasClass("x") && 
+	   $(squares[8]).hasClass("x")) {
+		return "x"
+	} 
+	if($(squares[0]).hasClass("o") && 
+	   $(squares[3]).hasClass("o") && 
+	   $(squares[6]).hasClass("o")) {
+		return "o"
+	}
+	if($(squares[1]).hasClass("o") && 
+	   $(squares[4]).hasClass("o") && 
+	   $(squares[7]).hasClass("o")) {
+		return "o"
+	}
+	if($(squares[2]).hasClass("o") && 
+	   $(squares[5]).hasClass("o") && 
+	   $(squares[8]).hasClass("o")) {
+		return "o"
+	} 
+	return null
+};
 
+var checkDiagonal = function(squares){
+	if($(squares[0]).hasClass("x") && 
+	   $(squares[4]).hasClass("x") && 
+	   $(squares[8]).hasClass("x")) {
+		return "x"
+	}
+	if($(squares[2]).hasClass("x") && 
+	   $(squares[4]).hasClass("x") && 
+	   $(squares[6]).hasClass("x")) {
+		return "x"
+	}
+	if($(squares[0]).hasClass("o") && 
+	   $(squares[4]).hasClass("o") && 
+	   $(squares[8]).hasClass("o")) {
+		return "o"
+	}
+	if($(squares[2]).hasClass("o") && 
+	   $(squares[4]).hasClass("o") && 
+	   $(squares[6]).hasClass("o")) {
+		return "o"
+	}
+
+	return null
 };
 
 
