8ba4840a4a81204b921f56e7b4aae50bb6d819c6
Tiffany W <tiffany.wang777@gmail.com>
2015-04-29 13:33:05 -0700

    Implement abstract board and drawBoard function.

--- connect4.js
+++ connect4.js
@@ -1,44 +1,50 @@
-var drawMove = function(square, move){
-	$(square).removeClass("blank").addClass(move);
-};
+$(document).ready(function() {
+	drawBoard();
+});
 
 var generateComputerMove = function(){
-	var blankSquares = $(".blank.square");
-	var squares = $(".square");
-	var firstMove = blankSquares[Math.floor(Math.random()*blankSquares.length)];
-	return firstMove;
+
 };
 
 var newGame = function(){
-	$(".blank").click(function(){
-		drawMove(this, "red");
-		var computerMove = generateComputerMove();
-		drawMove(computerMove, "yellow");
-	});
+
 };
 
-var board = [[null,null,null,null,null,null,null], 
-			 [null,null,null,null,null,null,null], 
-			 [null,null,null,null,null,null,null], 
-			 [null,null,null,null,null,null,null], 
-			 [null,null,null,null,null,null,null], 
-			 [null,null,null,null,null,null,null]];
+var board = [[null,"yellow",null,null,null,null], 
+			 [null,null,null,null,null,null], 
+			 [null,null,null,null,null,null], 
+			 [null,null,null,null,null,null], 
+			 [null,null,null,null,null,null], 
+			 [null,null,null,null,null,"red"],
+			 [null,null,null,null,null,null]];
+
+var drawSquare = function(x,y,symbol) {
+    if(symbol === null) symbol = "blank";
+    var squareSelector = "[data-index-x='"+ x + "'][data-index-y='"+ y + "']";
+    var square = $(squareSelector);
+    square.removeClass("blank");
+    square.removeClass("red");
+    square.removeClass("yellow");
+    square.addClass(symbol);
+};
 
 var drawBoard = function(){
 	var boardUI = $(".board")
-	$.each(this.board, function(x,row)) {
-		$.each(row,function(y,square){
-			this.drawSquare(x,y,square);
+	$.each(this.board, function(x,column) {
+		$.each(column,function(y,square){
+			drawSquare(x,y,square);
 		});
-	};
+	});
 };
 
+/*
 var computerChoice = Math.random();
 if(computerChoice < 0.34) {
-	computerChoice = /* computer picks the a random square between 35 and 41*/
+	computerChoice = // computer picks the a random square between 35 and 41
 } else if(computerChoice <=)
 
 $(document).ready(function(){
 	newGame();
 
 });
+*/
